b0562c672edcf9bbee92d32305b81419547a4119
Susan Tan <ssusantan9@gmail.com>
2019-04-22 01:52:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do 
+        "My name is Susannn."
+    end
+    
+    get "/hometown" do 
+        "My hometown is Bklyn."
+    end 
+
+    get "/favorite-song" do 
+        "My favorite song is song."
+    end 
+
 end
